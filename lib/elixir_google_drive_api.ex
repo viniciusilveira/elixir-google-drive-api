cbac04777171efa1851576d51f11e253c65a9f04
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-10-31 20:52:07 -0200

    init project

---
+++ lib/elixir_google_drive_api.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGoogleDriveApi do
+  @moduledoc """
+  Documentation for ElixirGoogleDriveApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGoogleDriveApi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_google_drive_api_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGoogleDriveApiTest do
+  use ExUnit.Case
+  doctest ElixirGoogleDriveApi
+
+  test "greets the world" do
+    assert ElixirGoogleDriveApi.hello() == :world
+  end
+end
