c986336a518001e2488f3fec4196044861e0898b
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-11-02 16:23:12 -0200

    Add mock and plug deps

--- mix.exs
+++ mix.exs
@@ -21,7 +21,9 @@ defmodule ElixirGoogleDriveApi.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:goth, "~> 0.4.0"}
+      {:plug, "~> 1.0"},
+      {:goth, "~> 0.4.0"},
+      {:mock, "~> 0.2.0", only: :test}
     ]
   end
 end
