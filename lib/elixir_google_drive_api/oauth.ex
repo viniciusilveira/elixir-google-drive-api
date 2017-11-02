3c1c346e3af5d0e68c7caafec476c852f0896a10
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-11-02 13:05:30 -0200

    Fix token return

--- lib/elixir_google_drive_api/oauth.ex
+++ lib/elixir_google_drive_api/oauth.ex
@@ -2,7 +2,7 @@ defmodule ElixirGoogleDriveApi.Oauth do
   @auth_scope "https://www.googleapis.com/auth/drive"
 
   def token do
-    {:ok, token} = Goth.Token.for_scope(@auth_scope)
+    {:ok, %{token: token}} = Goth.Token.for_scope(@auth_scope)
     token
   end
 end
