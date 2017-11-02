ba2b554793f062a7b8439bece3829d355d22ec6b
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-11-02 17:38:33 -0200

    Add package settings

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ElixirGoogleDriveApi.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      package: package(),
+      description: description(),
+      deps: deps(),
+      source_url: "https://github.com/viniciusilveira/elixir-google-drive-api"
     ]
   end
 
@@ -25,6 +28,21 @@ defmodule ElixirGoogleDriveApi.Mixfile do
       {:goth, "~> 0.4.0"},
       {:mock, "~> 0.2.0", only: :test},
       {:credo, "~> 0.8.1", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+    ]
+  end
+
+  def description do
+    "Elixir library to work files of Google Drive"
+  end
+
+  defp package do
+    [
+      name: "elixir_google_drive_api",
+      files: ["lib", "mix.exs", "README*" ],
+      maintainers: ["Marcos Vinicius O. Silveira"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/viniciusilveira/elixir-google-drive-api"}
     ]
   end
 end
