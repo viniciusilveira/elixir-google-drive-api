afdb55a24795c0ff34b5110d3e0803a06d0da963
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-11-02 11:30:01 -0200

    Implements authentication

---
+++ lib/elixir_google_drive_api/oauth.ex
@@ -0,0 +1,8 @@
+defmodule ElixirGoogleDriveApi.Oauth do
+  @auth_scope "https://www.googleapis.com/auth/drive"
+
+  def token do
+    {:ok, token} = Goth.Token.for_scope(@auth_scope)
+    token
+  end
+end
