75eb37676ea79e89bb92fc4f0a17c09bd3e31fab
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-11-26 18:38:27 -0200

    Add export_file function

--- lib/elixir_google_drive_api/drive.ex
+++ lib/elixir_google_drive_api/drive.ex
@@ -1,7 +1,12 @@
 defmodule ElixirGoogleDriveApi.Drive do
+  alias File
+
   defp copy_url(file_id), do: "https://www.googleapis.com/drive/v3/files/#{file_id}/copy"
 
   defp update_url(file_id), do: "https://www.googleapis.com/drive/v2/files/#{file_id}"
+
+  defp export_url(file_id, mime_type), do:
+    "https://www.googleapis.com/drive/v3/files/#{file_id}/export?mimeType=#{mime_type}"
 
   defp mount_body(%{title: title}) do
     %{title: title}
@@ -25,6 +30,15 @@ defmodule ElixirGoogleDriveApi.Drive do
       {:ok, %HTTPoison.Response{body: response_body}} ->
         response_body
         |> Poison.decode!
+      {:error, %HTTPoison.Error{reason: reason}} -> reason
+    end
+  end
+
+  def export_file(file_id, headers, mime_type \\ "application/vnd.oasis.opendocument.text", download_dest \\ ".temp/doc_file.doc") do
+    case HTTPoison.get export_url(file_id, mime_type), headers do
+      {:ok, %HTTPoison.Response{body: response_body}} ->
+        File.write!(download_dest, response_body)
+        download_dest
       {:error, %HTTPoison.Error{reason: reason}} -> reason
     end
   end
