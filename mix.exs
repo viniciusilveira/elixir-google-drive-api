37366c9f563a797c48f488e01d6889559ca8ecf6
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2017-12-05 13:15:10 -0200

    create tag 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirGoogleDriveApi.Mixfile do
   def project do
     [
       app: :elixir_google_drive_api,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       package: package(),
