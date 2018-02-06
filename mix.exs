a56cef51f0f61ef1815e08a2a2fd7b1c06df48a1
Marcos Vinicius O. Silveira <viniciussilveira6@gmail.com>
2018-02-06 17:26:29 -0200

    Changes version to 0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirGoogleDriveApi.Mixfile do
   def project do
     [
       app: :elixir_google_drive_api,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       package: package(),
