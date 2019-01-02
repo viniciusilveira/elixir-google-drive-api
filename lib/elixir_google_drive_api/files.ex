868a3e757c8b71fa4ba20bd20bd2185e16dbc2de
viniciusilveira <viniciussilveira6@gmail.com>
2019-01-02 14:00:36 -0200

    Add documentation for Files and Permissions modules

--- lib/elixir_google_drive_api/files.ex
+++ lib/elixir_google_drive_api/files.ex
@@ -15,15 +15,60 @@ defmodule ElixirGoogleDriveApi.Files do
   end
   defp mount_body(_), do: %{} |> Poison.encode!
 
+  @doc"""
+  Update file name
+
+  ### Examples
+
+    iex>rename_file(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      %{title: "new name"},
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ]
+    )
+    {:ok, response_body}
+  """
   def rename_file(file_id, opts = %{title: _name}, headers) do
     body = mount_body(opts)
     request(:patch, update_url(file_id), headers, body)
   end
 
+
+  @doc"""
+  Update file name
+
+  ### Examples
+
+    iex>rename_file(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ]
+    )
+    {:ok, response_body}
+  """
   def copy_file(file_id, headers) do
     request(:post, copy_url(file_id), headers)
   end
 
+
+  @doc"""
+  Update file name
+
+  ### Examples
+
+    iex>rename_file(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ]
+    )
+    {:ok, ".temp/doc_file.doc"}
+  """
   def export_file(file_id, headers, mime_type \\ "application/vnd.oasis.opendocument.text", download_dest \\ ".temp/doc_file.doc") do
     case HTTPoison.get export_url(file_id, mime_type), headers do
       {:ok, %HTTPoison.Response{body: "Not Found"}} -> {:error, :not_found}
--- lib/elixir_google_drive_api/permissions.ex
+++ lib/elixir_google_drive_api/permissions.ex
@@ -1,4 +1,7 @@
 defmodule ElixirGoogleDriveApi.Permissions do
+  @moduledoc """
+  This module for managing files permissions
+  """
   alias File
 
   import ElixirGoogleDriveApi.Service
@@ -8,14 +11,61 @@ defmodule ElixirGoogleDriveApi.Permissions do
 
   defp insert_permission_url(file_id), do: "#{api_url(file_id)}/permissions"
 
+  @doc"""
+  Update permission for one file
+
+  ## Examples
+
+    iex> update_permission_file(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ],
+      writer
+    )
+    {:ok, response_body}
+
+  """
   def update_permission_file(file_id, headers, permission_id) do
     request(:put, update_permission_url(file_id, permission_id), headers)
   end
 
+  @doc"""
+  Insert new file permission
+
+  ## Examples
+
+    iex> insert_permission_file(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ],
+      "{\"type\":\"anyone\",\"role\":\"writer\"}"
+    )
+    {:ok, response_body}
+
+  """
   def insert_permission_file(file_id, headers, body \\ "") do
     request(:post, insert_permission_url(file_id), headers, body)
   end
 
+  @doc"""
+  Share file for everyone with link
+
+  ## Examples
+
+    iex> share_file_with_link(
+      "12BbXrA627UzddYJWK3qww1yvWl_aKuvEeSv-qkPtz68",
+      [
+        "Content-Type": "application/json",
+        "Authorization": "Bearer ya29.Glv3BDWcd49KSskqCfSKgd_DURWF7VAbtr_b_tLneBgJZE-TCkZgwauSBIM1rf_tt344YaIgP_na-Xt5U0azKjSFTmt9_YwgfweC_b_ndyZ6Nz1PtdHXaFpOO50C"
+      ],
+    )
+    {:ok, response_body}
+
+  """
   def share_file_with_link(file_id, headers) do
     body =
       %{
